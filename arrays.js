b2ad298b577c8a5f7bbc6221b774c29291e1f2a7
orishaba-mercy <orishabamercybanga@gmail.com>
2023-03-17 19:44:14 +0300

    first

---
+++ arrays.js
@@ -0,0 +1,36 @@
+
+// Find the last element of the following arrays.
+// arr1 = [3,7,34,90,12]
+// arr2 = [true, "green", "where",12,56]
+let array1 =[3,7,34,90,12];
+console.log(array1.slice(-1));
+
+let array2 = [true,"green","where",12,56];
+console.log(array2.slice(-1));
+
+
+
+// Write a JS program that will join the following array elements into a string
+ const myPets = ["Cow", "Python", "Snake", "Dog"];
+ console.log(myPets.join(''))
+
+
+//  Write a JS script to sort the following array items
+
+ let array3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+ console.log(array3.sort())
+
+ 
+//  Write a program to remove duplicates from the following array.
+//  Console the array without duplicates, and console another array that only contains the duplicates
+let array4 = ["apple", "mango", "apple",
+            "orange", "mango", "mango"];
+  console.log(array4.filter(index)) ;   
+  
+//   Write a JS script to search for the following word in the array.
+4
+// If the word is present, console it else console "the search word was not found"
+let arr5 = ["the", "way", "x", 4, 23];
+
+// rite a JS script to sort the following string
+// let word = "lufituaeb"
