a52fe725b4b9b2afbfe03f1afc3086ca1394e9f8
orishaba-mercy <orishabamercybanga@gmail.com>
2023-03-17 19:57:30 +0300

    first commit

--- arrays.js
+++ arrays.js
@@ -25,12 +25,14 @@ console.log(array2.slice(-1));
 //  Console the array without duplicates, and console another array that only contains the duplicates
 let array4 = ["apple", "mango", "apple",
             "orange", "mango", "mango"];
-  console.log(array4.filter(index)) ;   
+//   console.log(array4.newSet(Array)) ;   
   
 //   Write a JS script to search for the following word in the array.
 4
 // If the word is present, console it else console "the search word was not found"
 let arr5 = ["the", "way", "x", 4, 23];
+console.log(arr5.includes(4));
 
 // rite a JS script to sort the following string
-// let word = "lufituaeb"
+ let word = "lufituaeb"
+console.log(word.split('').sort().join(''));
